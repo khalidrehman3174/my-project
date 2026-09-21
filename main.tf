e07bc8be1caa2e0825483ec7671379422943b0b1
khalid rehman <khalid.rehman3174@gmail.com>
2026-09-21 17:36:41 +0100

    Add S3 bucket Terraform code

---
+++ main.tf
@@ -0,0 +1,17 @@
+   terraform {
+     required_providers {
+       aws = {
+         source  = "hashicorp/aws"
+         version = "~> 5.0"
+       }
+     }
+   }
+
+   provider "aws" {
+     region  = "eu-west-2"
+     profile = "practice"
+   }
+
+   resource "aws_s3_bucket" "demo" {
+     bucket = "khalid-my-project-20260921"
+   }
